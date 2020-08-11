2c6c70a4605d1e2302bff8b77e4e7c1ec9b3cca8
Rohit <rohitpal123@gmail.com>
2020-08-12 04:42:36 +0530

    Renamed the app and created server.js file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-training';
+  title = 'angular-reddit';
   option = "";
 
   optionChanged(option: string){
